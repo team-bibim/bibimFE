acbb099e8310bbc607bd369b4fbe45cb68063418
dalsu0222 <dalsu0222@sju.ac.kr>
2023-08-22 16:59:05 +0900

    운동 추가 구현

--- src/views/routine-write.vue
+++ src/views/routine-write.vue
@@ -11,13 +11,39 @@
                         </v-list-subheader>
                         <input class="search-bar" v-model="titleInput" placeholder="제목" @keyup.enter="searchBarInput">
                         <div class="routine-add-container">
-                            <div class="day-routine-box"></div>
+                            <div class="day-routine-box">
+                                <div class="day-routine-box-title">Day 1</div>
+                                <!-- <div class="day-routine-subbox">
+                                    <div class="exercise-title">바벨 숄더 프레스</div>
+                                    <div class="exercise-time">30m</div>
+                                </div> -->
+                                <div v-for="(exercise, index) in exercises" :key="index" class="day-routine-subbox">
+                                    <input v-model="exercise.title" class="exercise-title" placeholder="운동 이름">
+                                    <input v-model="exercise.time" class="exercise-time" placeholder="시간">
+                                </div>
+                                <button v-if="exercises.length < 5" class="day-routine-subbox" @click="addExercise">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23"
+                                        fill="none">
+                                        <path
+                                            d="M23 13.1429H13.1429V23H9.85714V13.1429H0V9.85714H9.85714V0H13.1429V9.85714H23V13.1429Z"
+                                            fill="#CCCCCC" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <button class="day-routine-box" style="display:flex;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23"
+                                    fill="none">
+                                    <path
+                                        d="M23 13.1429H13.1429V23H9.85714V13.1429H0V9.85714H9.85714V0H13.1429V9.85714H23V13.1429Z"
+                                        fill="#CCCCCC" />
+                                </svg>
+                            </button>
 
                         </div>
                         <div class="routine-end-container">
                             <!-- enter가 가능하도록 input태그 대신 다른 태그 사용 필요 -->
-                            <input class="explanation-bar" v-model="expInput" placeholder="설명"
-                                @keyup.enter="ExplanationInput">
+                            <textarea class="explanation-bar" v-model="expInput" placeholder="설명"
+                                @keyup.enter="ExplanationInput"></textarea>
 
                             <v-btn class="r-submit-button" color="#CD4444" @click="submitData">작성 완료</v-btn>
                             <!-- Add this button -->
@@ -80,6 +106,16 @@ const links = [
     ['mdi-alert-octagon', '내 루틴 보관함'],
     ['mdi-alert-octagon', '설정'],
 ]
+
+const exercises = ref([
+    { title: '바벨 숄더 프레스', time: '30m' }
+]);
+
+const addExercise = () => {
+    if (exercises.value.length < 5) {
+        exercises.value.push({ title: '', time: '' });
+    }
+};
 
 const drawer = ref(null)
 
@@ -165,6 +201,11 @@ export default {
     height: 178px;
     text-indent: 10px;
     margin: 10px 35px;
+    padding-top: 20px;
+
+    /* Prevent users from resizing the textarea */
+    resize: none;
+
 }
 
 .search-bar::placeholder {
@@ -180,7 +221,7 @@ export default {
     font-weight: bold;
     text-align: left;
     margin-right: 12px;
-    padding-top: 10px;
+    /* padding-top: 10px; */
 }
 
 .right-panel-hot-classify-text {
@@ -246,6 +287,8 @@ export default {
     text-align: center;
 }
 
+/* ---------------------------------------------------------------- */
+
 .routine-add-container {
     display: flex;
     justify-content: center;
@@ -263,12 +306,62 @@ export default {
     background: #4C6672;
 }
 
-.routine-add-container {
-    display: flex;
-    /* width: 1088px; */
-    align-items: flex-start;
-    gap: 12px;
+.day-routine-box-title {
+    color: #FFF;
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+
+    margin: 25px;
+
 }
+
+.exercise-title,
+.exercise-time {
+    color: #FFF;
+    font-family: Inter;
+    font-size: 17px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+
+.day-routine-subbox {
+    display: flex;
+    padding: 14px 25px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 7px;
+    border-radius: 20px;
+    background: #27373E;
+
+    width: 189px;
+    height: 84px;
+    margin: 10px auto;
+}
+
+.day-routine-subbox-plus {
+    display: flex;
+    padding: 14px 25px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 7px;
+    border-radius: 20px;
+    background: #27373E;
+
+    width: 189px;
+    height: 84px;
+    margin: 10px auto;
+}
+
+svg {
+    margin: auto;
+}
+
+/* ---------------------------------------------------------------- */
+
 
 .routine-end-container {
     display: flex;
