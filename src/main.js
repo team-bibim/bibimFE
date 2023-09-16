6d2171556c96b6d0460d7c7bd3f40e479810f960
dalsu0222 <dalsu0222@sju.ac.kr>
2023-09-16 17:39:52 +0900

    axios 이용하여 연동 연습

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import axios from "axios";
 
 loadFonts()
 
@@ -12,3 +13,13 @@ createApp(App)
   .use(store)
   .use(vuetify)
   .mount('#app')
+
+let url = "http://192.168.8.196:8000/exercise/1/"
+
+axios.get(url)
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(response) {
+    console.log(response);
+  })
