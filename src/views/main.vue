30030102b2e4b9df1f5fcdb6a6fd9a9329424209
우희경 <hk6558@naver.com>
2024-01-08 15:11:26 +0900

    mypage

--- src/views/main.vue
+++ src/views/main.vue
@@ -22,11 +22,11 @@
 </div>
 
 <!-- 모달내용 -->
-    <div class="modal fade" id="routinedatamodal" tabindex="-1"> <!--@show="onModalShow"-->
+    <div class="modal fade" id="routinedatamodal" tabindex="-1"> 
         <div class="modal-dialog modal-dialog-centered modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">내가 담은 루틴</h5> <!--id="routinedatamodal"-->
+                    <h5 class="modal-title">내가 담은 루틴</h5> 
                 </div>
             <div class="modal-body" id="routin-table">
                 <table class="table">
@@ -47,9 +47,6 @@
                             <td>
                                 <button @click="showRoutineId(routine)">더보기</button>
                             </td>
-                            <!-- <td>
-                                <button @click="editRoutine(routine)">수정버튼</button>
-                            </td> -->
                         </tr>
                     </tbody>  
                 </table>
@@ -114,8 +111,8 @@ const fetchRoutineData = async () => {
 }
 const showRoutineId = async (routine) => {
   try {
-    const routineId = routine.routine; // Assuming routine ID is extracted correctly
-    const apiUrl = `/api/routine/check/${routineId}/`; // Your API address
+    const routineId = routine.routine; 
+    const apiUrl = `/api/routine/check/${routineId}/`; 
 
     const response = await axios.get(apiUrl);
     console.log('Response from fetchRoutineId:', response);
@@ -125,7 +122,6 @@ const showRoutineId = async (routine) => {
       return;
     }
 
-    // Display routine details in the modal
     console.log('Routine ID:', response.data.routine_id);
     console.log('Routine Name:', response.data.routine_name);
 
@@ -139,19 +135,6 @@ const showRoutineId = async (routine) => {
 const goBack = () => {
   selectedRoutine.value = null;
 };
-
-// const router = useRouter();
-
-// const editRoutine = async (routine) => {
-//   try {
-//     const routineId = routine.routine;
-//     // Assuming your edit route is named 'edit'
-//     router.push({ name: 'edit', params: { id: routineId } });
-//   } catch (error) {
-//     console.error('Error navigating to edit page:', error);
-//   }
-// };
-
 
 </script>
 
--- src/views/settings.vue
+++ src/views/settings.vue
@@ -1,12 +1,154 @@
 <template>
-    <!--작성-->
-    This is a brocoli settings page.
-</template>
+<div id="back">
+<div id="back-round">
+    <!-- <div class="t"><p>태현이 자리</p></div> -->
+        <!-- <div class="line"></div> -->
+    <div class="open-switch">
+        <div class="open-s-text">계정 공개 여부</div>
+        <v-switch v-model="model" hide-details inset @change="updateAccountVisibility" 
+        style="transform: scale(1.2);"></v-switch>
+    </div>
+    <div class="line"></div>
+    <div class="myroutine">
+        <div class="myroutine-text">내 루틴</div>
+        <v-table id="tb" class="custom-table" style="padding: 20px;">
+        <thead>
+            <tr>
+                <th class="text-left" style="font-weight: bold;">
+                    #
+                </th>
+                <th class="text-left" style="font-weight: bold;">
+                    루틴제목
+                </th>
+                <th class="text-left" style="font-weight: bold;">
+                    루틴설명
+                </th>
+                <th class="text-left">
+                    
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, index) in routines" :key="item.routine_name">
+                <td>{{ index + 1 }}</td>
+                <td>{{ item.routine_name }}</td>
+                <td>{{ item.routine_comment }}</td>
+                <td>
+                    <v-btn variant="tonal" style="background-color: #CD4444;">수정</v-btn>
+                </td>
+            </tr>
+        </tbody>
+        </v-table>
+    </div>
+</div>
+</div>
+</template> 
 
 <script>
-    // 작성
+import axios from 'axios';
+
+export default {
+data() {
+    return {
+    model: true,
+    routines: [],
+    };
+},
+mounted() {
+    this.fetchRoutineData();
+},
+methods: {
+    async fetchRoutineData() {
+        try {
+            const response = await axios.get('/api/routine/check/mine/');
+            this.routines = response.data;
+        } catch (error) {
+            console.error('Error fetching routine data:', error);
+        }
+    },
+    async updateAccountVisibility() {
+        try {
+            await axios.put('/api/accounts/info/', {
+                accVisibility: this.model ? 1 : 0,
+            });
+
+            console.log('Update success:', response.data);
+
+        } catch (error) {
+            // console.error('Error updating account visibility', error);
+            if (error.response) {
+      console.error('Server responded with error status:', error.response.status);
+      console.error('Error details:', error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error('No response received from the server');
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error setting up the request:', error.message);
+    }
+        }
+    },
+},
+};
 </script>
 
 <style>
-    /* 작성 */
+#back {
+    position: relative;
+    background-color: #181B21;
+    border-radius: 20px;
+    height: 500px;
+    align-items: center;
+}
+
+
+.t {
+    color: white;
+}
+
+.line {
+    text-align: center;
+    border-bottom: 1px solid #4B8AAF;
+    margin-left: 30px;
+    margin-right: 30px;
+} 
+
+.open-s-text {
+    margin-left:30px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    color: white;
+    font-size: 30px;
+    font-weight: bold;
+}
+
+.myroutine-text {
+    color: white;
+    font-size: 30px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    margin-left:30px;
+    font-weight: bold;
+}
+
+.open-switch {
+    display: flex;
+    align-items: center;
+    margin-left: auto
+}
+
+.v-switch {
+    margin-left: 1000px;
+}
+
+#tb { 
+    margin: 20px;
+    border-radius: 10px;
+}
+
+.custom-table {
+    background-color: #27373E;
+    color: white;
+}
+
 </style>
